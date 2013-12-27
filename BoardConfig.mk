1c272422a1e3f736f9a5dbc4928a98f4430879dc
Lars Greiss <kufikugel@googlemail.com>
2013-12-27 16:57:29 +0100

    hammerhead: switch to new toolchain path
    Change-Id: I7491452cae2772efb3051b08bbeda3291a5cfafe

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,7 +32,7 @@ BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
 TARGET_KERNEL_CONFIG := slim_hammerhead_defconfig
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro/bin/arm-linux-gnueabihf-
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.7-13.04/bin/arm-linux-gnueabihf-
 
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
