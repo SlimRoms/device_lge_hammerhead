988507e3b45a99ef6199dfa57edfa48f9e1c0ed6
myfluxi <linflux@arcor.de>
2015-02-06 01:16:41 +0100

    hammerhead: Use halfres boot animation
    Change-Id: Iaaac09628e2e484a32b92a4099b540b55cf9a479
    
    Conflicts:
    	cm.mk

--- slim.mk
+++ slim.mk
@@ -4,6 +4,7 @@ PRODUCT_RELEASE_NAME := hammerhead
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOTANIMATION_HALF_RES := true
 
 # Inherit some common Slim stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
