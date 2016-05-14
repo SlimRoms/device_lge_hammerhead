3d47540ca8d091192d6bfbfbf0d8a9d53558fde1
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-05-14 21:59:49 +0200

    update build fingerprint to MOB30H
    Change-Id: I85f03a902c3b67fefcc7b477e20bacab83d36d5a
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- slim.mk
+++ slim.mk
@@ -43,5 +43,5 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
    PRODUCT_NAME=hammerhead \
-    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MOB30D/2704746:user/release-keys \
-    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MOB30D 2704746 release-keys"
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MOB30H/2751534:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MOB30H 2751534 release-keys"
