265ea90e8776ebaff83b09e3d4e00351d6544873
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2017-03-28 22:02:06 +0200

    Update build fingerprint to M4B30Z
    Change-Id: I0edfb0d89448aff43ae5cd51e86c242e85b51212
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- slim.mk
+++ slim.mk
@@ -39,6 +39,6 @@ PRODUCT_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-   PRODUCT_NAME=hammerhead \
-    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/M4B30X/3237893:user/release-keys \
-    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 M4B30X 3237893 release-keys"
+    PRODUCT_NAME=hammerhead \
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/M4B30Z/3437181:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 M4B30Z 3437181 release-keys"
